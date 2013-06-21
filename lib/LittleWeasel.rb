8e8cd77ae1ceaaa8ab33c697d75d0962e688e9ab
gangelo <web.gma@gmail.com>
2013-06-20 22:39:49 -0400

    Make gem standard "require 'GemName'" to use.

--- lib/LittleWeasel.rb
+++ lib/LittleWeasel.rb
@@ -1,5 +1,33 @@
+require 'singleton'
 require "LittleWeasel/version"
 
 module LittleWeasel
-  # Your code goes here...
+
+  class Checker
+    include Singleton
+
+    attr_reader :dictionary
+
+    def initialize
+      @dictionary = Hash.new(1)
+      load
+    end
+
+    def exists?(word)
+      dictionary.has_key? word
+    end
+
+    private
+
+    def dictionary_path
+      File.expand_path(File.dirname(__FILE__) + '/dictionary')
+    end
+
+    def load
+      File.open(dictionary_path) do |io|
+        io.each { |line| line.chomp!; @dictionary[line] = line }
+      end
+    end
+  end
+
 end
