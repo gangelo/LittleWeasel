4c8231e1ba2179e24c0a20582782667d2f4162d5
gangelo <web.gma@gmail.com>
2013-06-26 22:13:23 -0400

    Add global options and accept options to exist?
    Add options to return false when exist? is called for words comprised
    soely of single letters of the alphabet.

--- lib/LittleWeasel.rb
+++ lib/LittleWeasel.rb
@@ -8,13 +8,36 @@ module LittleWeasel
 
     attr_reader :dictionary
 
+    private
+
+    attr_reader :alphanet_exclusion_list
+
+    public
+
     def initialize
+      @options = {exclude_alphabet: false}
+      @alphabet_exclusion_list = %w{ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z }
       @dictionary = Hash.new(1)
       load
     end
 
-    def exists?(word)
+    def exists?(word, options=nil)
+      options = options || @options
+
+      return false if word.nil? || !word.is_a?(String)
+      return false if options[:exclude_alphabet] && word.length == 1 && @alphabet_exclusion_list.include?(word.upcase)
+
       dictionary.has_key? word
+    end
+
+    # {exclude_alphabet: true} will return exist? == false for a-z, A-Z
+    # {exclude_alphabet: false} will return exist? == true for a-z, A-Z
+    def options=(options)
+      @options = options
+    end
+
+    def options
+      @options
     end
 
     private
--- spec/checker/checker_spec.rb
+++ spec/checker/checker_spec.rb
@@ -18,6 +18,37 @@ describe LittleWeasel do
     @spell.exists?('appel').should == false
   end
 
+  it 'should return false for nil' do
+    @spell.exists?(nil).should == false
+  end
+
+  it 'should return false for non-String' do
+    @spell.exists?(:word).should == false
+  end
+
+  it 'should return true if option exclude_alphabet is false, and word is a letter' do
+    @spell.options = {exclude_alphabet: false}
+    @spell.exists?('x').should == true
+    @spell.exists?('X').should == true
+  end
+
+  it 'should return false if option exclude_alphabet is true, and word is a letter' do
+    @spell.options = {exclude_alphabet: true}
+    @spell.exists?('x').should == false
+    @spell.exists?('X').should == false
+  end
+
+  it 'should use options passed to exist? 1' do
+    @spell.options = {exclude_alphabet: false}
+    @spell.exists?('h', {exclude_alphabet: true}).should == false
+  end
+
+  it 'should use options passed to exist? 2' do
+    @spell.options = {exclude_alphabet: true}
+    @spell.exists?('h', {exclude_alphabet: false}).should == true
+  end
+
+  # TODO: Profile
   it "shouldn't take forever to find a lot of words" do
     words = %w{ all bad cap dad eat fad glad had inch jump kind lend monster on put quiet run sad tape under vector was xenophobe yes zebra }
 
