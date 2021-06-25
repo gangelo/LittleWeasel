3230cd5ae41e2c9a7c78ebeb56582863c0ef3c15
gangelo <web.gma@gmail.com>
2021-06-25 02:10:29 -0400

    Add yard comments

--- lib/LittleWeasel/configure.rb
+++ lib/LittleWeasel/configure.rb
@@ -18,15 +18,22 @@ module LittleWeasel
     attr_writer :configuration
   end
 
-  # This class holds the configuration properties for this gem.
+  # This class encapsulates the configuration properties for this gem and
+  # provides methods and attributes that allow for management of the same.
+  #
+  # attr_reader :max_dictionary_file_megabytes, :max_invalid_words_bytesize, :metadata_observers
   class Configuration
     attr_reader :max_dictionary_file_megabytes,
       :max_invalid_words_bytesize, :metadata_observers
 
+    # The constructor; calls {#reset}.
     def initialize
       reset
     end
 
+    # Resets the configuration settings to their default values.
+    #
+    # @return [void]
     def reset
       @max_dictionary_file_megabytes = 5
       @max_invalid_words_bytesize = 25_000
@@ -35,10 +42,20 @@ module LittleWeasel
       ]
     end
 
+    # Returns the maximum consumable dictionary size in bytes. Dictionaries
+    # larger than {#max_dictionary_file_bytes} will raise an error.
+    #
+    # The default is 5 megabytes.
+    #
+    # @return [Integer] the maximum number of bytes for a dictionary.
     def max_dictionary_file_bytes
       @max_dictionary_file_megabytes * 1_000_000
     end
 
+    # If {#max_invalid_words_bytesize} is > 0, true will be returned; false
+    # otherwise.
+    #
+    # @return [true, false] based on {#max_invalid_words_bytesize}.
     def max_invalid_words_bytesize?
       max_invalid_words_bytesize.positive?
     end
@@ -48,6 +65,13 @@ module LittleWeasel
       @max_dictionary_file_megabytes = value
     end
 
+    # Sets the maximum cache size (in bytes) for invalid words. If
+    # less than or equal to 0, invalid words will NOT be cached.
+    #
+    # If greater than 0, invalid words will be cached up to and including
+    # {#max_invalid_words_bytesize} bytes.
+    #
+    # @see #max_invalid_words_bytesize?
     def max_invalid_words_bytesize=(value)
       value = 0 if value.negative?
       @max_invalid_words_bytesize = value
